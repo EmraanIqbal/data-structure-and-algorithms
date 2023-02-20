ad2cf274a2891d8443eee6c480342c5e043788c3
EmraanIqbal <se.imraniqbal@gmail.com>
2023-02-20 14:42:25 +0500

    primality algorithm

--- data-structure-with-codeEvolution/9-PrimeNumber.js
+++ data-structure-with-codeEvolution/9-PrimeNumber.js
@@ -13,3 +13,21 @@ const primeNumber = (n) => {
 primeNumber(1);
 
 //  Big-O = O(n)
+
+// ------------------------------------------------- Primality Algorithm ---------------------------------------------------------
+
+const primeNumberPrimalarity = (n) => {
+  if (n < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    let sum = n % i;
+    if (sum == 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+primeNumber(1);
+
+//  Big-O = O(sqrrt(n))
