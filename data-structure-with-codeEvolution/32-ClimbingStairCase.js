895defa4d581711bead5e490cd74fcd36aa649b4
EmraanIqbal <se.imraniqbal@gmail.com>
2023-02-22 21:08:19 +0500

    miscelineous

---
+++ data-structure-with-codeEvolution/32-ClimbingStairCase.js
@@ -0,0 +1,18 @@
+function cliimbingStaircase(n) {
+  let noOfWays = [1, 2];
+
+  for (let i = 2; i <= 2; i++) {
+    noOfWays.push(noOfWays[i - 1] + noOfWays[i - 2]);
+  }
+
+  return noOfWays;
+}
+
+console.log(cliimbingStaircase(1));
+console.log(cliimbingStaircase(2));
+console.log(cliimbingStaircase(3));
+console.log(cliimbingStaircase(4));
+console.log(cliimbingStaircase(5));
+console.log(cliimbingStaircase(6));
+
+// Big-O = O(n)
