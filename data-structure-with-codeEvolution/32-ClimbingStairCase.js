c96839c09a685f462748050cfcc761dfb5a00f0a
EmraanIqbal <se.imraniqbal@gmail.com>
2023-02-22 21:10:01 +0500

    miscelineous

--- data-structure-with-codeEvolution/32-ClimbingStairCase.js
+++ data-structure-with-codeEvolution/32-ClimbingStairCase.js
@@ -2,7 +2,7 @@ function cliimbingStaircase(n) {
   let noOfWays = [1, 2];
 
   for (let i = 2; i <= n; i++) {
-    noOfWays.push(noOfWays[i - 1] + noOfWays[i - 2]);
+    noOfWays[i] = noOfWays[i - 1] + noOfWays[i - 2];
   }
 
   return noOfWays[n - 1];
