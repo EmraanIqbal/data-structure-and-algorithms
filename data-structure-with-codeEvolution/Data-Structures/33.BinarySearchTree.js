9d1c43fafcc889a09ac5c59f5f20d694c733ef73
EmraanIqbal <se.imraniqbal@gmail.com>
2023-02-28 18:45:56 +0500

    depth Search Tree

--- data-structure-with-codeEvolution/Data-Structures/33.BinarySearchTree.js
+++ data-structure-with-codeEvolution/Data-Structures/33.BinarySearchTree.js
@@ -39,10 +39,59 @@ class BinarySearchTree {
       }
     }
   }
+
+  search(root, value) {
+    if (!root) return console.log("Value is not found " + value);
+    else {
+      if (value === root.value) {
+        console.log("Expected Value is Found :", value);
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+
+  preorder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preorder(root.left);
+      this.preorder(root.right);
+    }
+  }
+
+  postorder(root) {
+    if (root) {
+      this.postorder(root.left);
+      this.postorder(root.right);
+      console.log(root.value);
+    }
+  }
+
+  inorder(root) {
+    if (root) {
+      this.inorder(root.left);
+      console.log(root.value);
+      this.inorder(root.right);
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+// bst.search(bst.root, 10);
+bst.search(bst.root, 25);
+// bst.search(bst.root, 15);
 console.log("Tree is Empty ?", bst.isEmpty());
+console.log("------------Pre-Order Traversal-------------");
+bst.preorder(bst.root);
+console.log("------------Post-Order Traversal-------------");
+bst.postorder(bst.root);
+console.log("------------In-Order Traversal-------------");
+bst.inorder(bst.root);
